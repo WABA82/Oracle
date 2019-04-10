36a7cacd798eb3a9dc1a3cb5498f0404720c7ced
WABA82 <43511145+WABA82@users.noreply.github.com>
2019-04-10 17:48:39 +0900

    커밋

--- PL1108/procedure_insert_test_proc.sql
+++ PL1108/procedure_insert_test_proc.sql
@@ -10,15 +10,15 @@
 -- 중복된 사원 번호가 입력되면 예외로 처리한다.
 create or replace procedure insert_test_proc(
 		empno number, ename varchar2, sal number,
-		job varchar2, out varchar2, row_cnt out number )
+		job varchar2, msg out varchar2, row_cnt out number )
 is          
 	temp_sal number := sal; 
 	
 begin    
 	row_cnt := 0;	
-
+    --1.
 	if empno  between 1 and 9999 then
-
+        --2.
 	    if temp_sal < 2500 then
 	         temp_sal := 2500;
 	    end if;        
@@ -26,7 +26,7 @@ begin
 	    if temp_sal > 8000 then
 	       temp_sal := 8000;
 	    end if;  
-	     
+	    --3. 
 	    if job in('사원', '주임', '대리', '과장', '차장', '부장', '이사') then
 	    	
 	    	insert into test_proc(empno, ename,hiredate,sal,job)
@@ -51,7 +51,7 @@ begin
 	when dup_val_on_index then
 		msg := empno||'번 사원번호는 이미 존재합니다.';
 	when others then
---		msg:='예외발생 : '||sqlerrm||' / '|| sqlcode;
+	-- msg:='예외발생 : '||sqlerrm||' / '|| sqlcode;
 	if sqlcode = -12899 then
 		msg := ename || '은 한글은 3자 까지 영어는 10까지만 입력 할 수 있습니다.';
 	end if;
